90548e7f726b90591fb16aab643de42f8e69001f
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-11 15:29:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,11 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_hash = {}
+  data.each{|key1, value1|
+    value1.each{|key2, value2|
+      value2.each{|names| sorted_hash[names] = {:color => [], :gender => [], :lives => []}
+      }
+    }
+  }
+  
+  sorted_hash
 end
