a18c5224a86e0db82af4b4b765ed5e4defc1e793
Alexander Mosquea <alexander.rob0@gmail.com>
2019-09-11 16:01:40 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  sorted_hash = {}
+  data.each{|key1, value1|
+    value1.each{|key2, value2|
+      value2.each{|names| sorted_hash[names] = {:color => [], :gender => [], :lives => []}
+      }
+    }
+  }
+  
+  data.each {|key, value|
+    data[key].each {|attribute, name|
+      name.each {|bird_name|
+        sorted_hash.keys.each {|item|
+          if bird_name === item
+            sorted_hash[item][key] << attribute.to_s
+          end
+        }
+      }
+    }
+  }
+  sorted_hash
 end
